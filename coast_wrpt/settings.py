6aaafd6eb0720e49a3c3477a12baba75437451db
Greg Janée <gregjanee@gmail.com>
2019-03-11 16:07:07 -0700

    Discovered a more direct way to serve robots.txt.

--- coast_wrpt/settings.py
+++ coast_wrpt/settings.py
@@ -57,6 +57,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "coast_wrpt", "static")]
+WHITENOISE_ROOT = os.path.join(BASE_DIR, "coast_wrpt", "static", "root")
 
 TEMPLATES = [
   { "BACKEND": "django.template.backends.django.DjangoTemplates",
--- coast_wrpt/urls.py
+++ coast_wrpt/urls.py
@@ -5,13 +5,11 @@
 # -----------------------------------------------------------------------------
 
 from django.contrib import admin
-from django.shortcuts import redirect
 from django.urls import include, path
 
 admin.autodiscover()
 
 urlpatterns = [
-  path("robots.txt", lambda request: redirect("/static/robots.txt")),
   path("", include("wrpt.urls")),
   path("admin/", admin.site.urls)
 ]
